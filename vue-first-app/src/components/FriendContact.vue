7476ca4137acf160957744bb832dfb94c9bd6909
Vangoda <vangoda@vangoda.com>
2022-12-20 11:56:03 +0000

    Finished lesson

---
+++ vue-first-app/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: true,
+      friend: {
+        id: "Manuel",
+        name: "Manuel Jackson",
+        phone: "01 234 5678",
+        email: "manuel@gmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
