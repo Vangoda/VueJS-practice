62c59c2c7a3003b94645fd527373c8ea1b56b230
Vangoda <vangoda@vangoda.com>
2023-01-16 19:16:11 +0000

    Added is favorite message and prop

--- vue-first-app/src/components/FriendContact.vue
+++ vue-first-app/src/components/FriendContact.vue
@@ -1,27 +1,42 @@
 <template>
   <li>
-    <h2>{{ name }}</h2>
+    <h2>{{ friend.isFavorite ? "Favorite friend" : "" }} {{ friend.name }}</h2>
+    <button @click="toggleFavorite">Toggle favorite</button>
     <button @click="toggleDetails">
       {{ detailsAreVisible ? "Hide" : "Show" }} details
     </button>
     <ul v-if="detailsAreVisible">
-      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
-      <li><strong>Email:</strong> {{ emailAddress }}</li>
+      <li><strong>Phone:</strong> {{ friend.phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ friend.emailAddress }}</li>
     </ul>
   </li>
 </template>
 
 <script>
 export default {
-  props: ["name", "phoneNumber", "emailAddress"],
+  props: {
+    name: String,
+    phoneNumber: String,
+    emailAddress: String,
+    isFavorite: String,
+  },
   data() {
     return {
       detailsAreVisible: false,
+      friend: {
+        name: this.name,
+        phoneNumber: this.phoneNumber,
+        emailAddress: this.emailAddress,
+        isFavorite: this.isFavorite,
+      },
     };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.friend.isFavorite = !this.friend.isFavorite;
     },
   },
 };
